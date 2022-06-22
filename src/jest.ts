7c2165162063b0c90f17ea44ed1313fcafa07b81
Jacob <68607796+skra-pa-pa@users.noreply.github.com>
2022-06-22 17:38:42 -0400

    fix jest 28 support

--- src/jest.ts
+++ src/jest.ts
@@ -2,7 +2,7 @@ import { transpile } from "./esbuild"
 import "./register"
 
 function process(src: string, filename: string) {
-  return transpile(src, filename, { type: "transform" })
+  return { code: transpile(src, filename, { type: "transform" }) }
 }
 
 export default { process }
